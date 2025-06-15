1fe3930341f8b792f2802f7a06c48aa817dfa99f
Polcia <witkowska.pola@gmail.com>
2025-06-15 22:33:17 +0200

    Add Kubernetes configurations for backend and frontend services

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ async function fetchCosmetics() {
     const token = document.cookie;
     const current_user = getCurrentUser();
     const response = await fetch(
-      `http://127.0.0.1:5000/users/${current_user}/saved_cosmetics`,
+      `${process.env.REACT_APP_API_URL}/users/${current_user}/saved_cosmetics`,
       {
         method: "GET",
         headers: {
